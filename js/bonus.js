0e323a36055697bff78807efebf26490e2515669
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-15 17:35:04 +0100

    // Crea 10 oggetti che rappresentano una zucchina.

--- js/bonus.js
+++ js/bonus.js
@@ -1,5 +1,68 @@
 // BONUS 
  
 // Crea 10 oggetti che rappresentano una zucchina.,
+const zucchine = [
+    
+    { 
+        varietà: "verdi", 
+        peso: 0.5, 
+        lunghezza: 15
+    },
+    
+    {
+        varietà: "chiare", 
+        peso: 1, 
+        lunghezza: 10
+    },
+    
+    {
+        varietà: "tonde", 
+        peso: 1.1, 
+        lunghezza: 11
+    },
+    
+    {
+        varietà: "lunghe-fiorentine", 
+        peso: 1.8, 
+        lunghezza: 12
+    },
+    
+    {
+        varietà: "trombetta", 
+        peso: 1.4, 
+        lunghezza: 13
+    },
+    
+    {
+        varietà: "gialle", 
+        peso: 1.5, 
+        lunghezza: 14
+    },
+    
+    {
+        varietà: "siciliane", 
+        peso: 1.7, 
+        lunghezza: 15
+    },
+    
+    {
+        varietà: "bianche-triestine", 
+        peso: 1.6, 
+        lunghezza: 16
+    },
+    
+    {
+        varietà: "patisson", 
+        peso: 1.3, 
+        lunghezza: 17
+    },
+    
+    {
+        varietà: "crookneck", 
+        peso: 1.2, 
+        lunghezza: 18
+    }
+];
+
 // Dividi in due array separati le zucchine che misurano meno o più di 15cm.,
 // Infine stampa separatamente quanto pesano i due gruppi di zucchine.,
