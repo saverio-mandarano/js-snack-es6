b2aee272db040c1f90830ab017ec8f2cccff7509
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-15 15:35:36 +0100

    // Snack 1 // Creare un array di oggetti: // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.

---
+++ js/snack-1.js
@@ -0,0 +1,57 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+const bikeObjects = [
+    
+    { 
+        nome: `Trek Domane SL 6`, 
+        peso: 8.9 
+    },
+    
+    { 
+        nome: `Specialized Rockhopper`, 
+        peso: 13.2 
+    },
+    
+    { 
+        nome: `Cannondale Trail 5`, 
+        peso: 13.5 
+    },
+    
+    { 
+        nome: `Bianchi Oltre XR4`, 
+        peso: 6.8 
+    },
+    
+    { 
+        nome: `Giant Talon 2`, 
+        peso: 13.0 
+    },
+    
+    { 
+        nome: `Scott Spark 970`, 
+        peso: 12.6 
+    },
+    
+    { 
+        nome: `Cube Reaction Pro`, 
+        peso: 11.9 
+    },
+    
+    { 
+        nome: `Orbea Orca M30`, 
+        peso: 8.4 
+    },
+    
+    { 
+        nome: `Santa Cruz Hightower`, 
+        peso: 14.2 
+    },
+    
+    { 
+        nome: `Pinarello Dogma F`, 
+        peso: 6.8 
+    }
+];
+
