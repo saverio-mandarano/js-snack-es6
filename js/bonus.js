4dc7651351f4dc3794ae503dace59bb0b7e7d9c9
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-15 17:33:45 +0100

    start bonus

---
+++ js/bonus.js
@@ -0,0 +1,5 @@
+// BONUS 
+ 
+// Crea 10 oggetti che rappresentano una zucchina.,
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm.,
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.,
